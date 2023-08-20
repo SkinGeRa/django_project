baf2879225a44d86de197d9e06f3c1718cbd22b3
Nickolay <psyangel93@mail.ru>
2023-08-20 23:00:25 +0300

    Add file

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 
+# Create your views here.
 def contacts(request):
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contact.html')
 
 
 def home(request):
