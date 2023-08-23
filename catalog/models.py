74bd4f67c3c1cf75d12ddc01df734e28bf3df540
Nickolay <psyangel93@mail.ru>
2023-08-23 23:40:29 +0300

    update file

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, verbose_name='наименование')
+    category_description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.category_name} {self.category_description}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('category_name',)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='наименование')
+    product_description = models.TextField(verbose_name='описание')
+    product_image = models.ImageField(upload_to='product/', verbose_name='изображение', **NULLABLE)
+    product_category = models.CharField(max_length=30, verbose_name='категория')
+    product_purchase_price = models.IntegerField(verbose_name='цена')
+    product_date_of_create = models.DateField(verbose_name='дата создания')
+    product_last_modified_date = models.DateField(verbose_name='дата изменения', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.product_name}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('product_name',)
+
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_project',
+        'PASSWORD': '12345'
     }
 }
 
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
