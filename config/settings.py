1f3c7f1c2cf8c183cf129d47f475d31be23e3c59
n.geraskin <psyangel93@mail.ru>
2023-08-24 14:06:56 +0300

    Update file

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
         'NAME': 'django_project',
-        'PASSWORD': '12345'
+        'PASSWORD': '123'
     }
 }
 
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
