29ff324296b139e4fe9b0d987e5ca77acb6b7ad4
n.geraskin <psyangel93@mail.ru>
2023-09-07 17:56:45 +0300

    update file

--- catalog/views.py
+++ catalog/views.py
@@ -10,7 +10,7 @@ def index(request):
         'object_list': product_list,
         'title': 'Каталог товаров'
     }
-    return render(request, 'catalog/product.html', context)
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
@@ -21,3 +21,10 @@ def home(request):
     return render(request, 'catalog/home.html')
 
 
+def product(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'object_list': product_item,
+        'title': f'Категория товара - {product_item.product_name}'
+    }
+    return render(request, 'catalog/product.html', context)
